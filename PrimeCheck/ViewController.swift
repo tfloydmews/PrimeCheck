b8b1a90dddc3f20ca208289184ff59923d2055c0
Taylor <tfloydmews@Taylors-iMac.local>
2017-02-01 00:15:38 -0600

    Initial Commit

---
+++ PrimeCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeCheck
+//
+//  Created by Taylor on 2/1/17.
+//  Copyright © 2017 Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
