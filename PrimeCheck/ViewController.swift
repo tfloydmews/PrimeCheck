b1670bc21a7e0090b25a4f54d86c38d4f71eab13
tfloydmews <tfloydmews@Taylors-iMac.local>
2017-02-02 16:29:38 -0600

    Lab 1 Prime Checker

--- PrimeCheck/ViewController.swift
+++ PrimeCheck/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberInput: UITextField!
+    @IBOutlet weak var primeLabel: UILabel!
+    
+    @IBAction func checkPrime(_ sender: UIButton) {
+        let number:Int? = Int(numberInput.text!)
+        if number == nil {
+            primeLabel.text = "Enter Number"
+            return
+        }
+        
+        if number! <= 1 {
+                self.primeLabel.text = "Not Prime"
+        }
+        else {
+            if number! <= 3 {
+                self.primeLabel.text = "Prime"
+            }
+        }
+        
+            
+        var i = 2
+        while i*i <= number! {
+            if number! % i == 0 {
+                primeLabel.text = "Composite"
+                break
+            }
+            else {
+                if number! % i > 0 {
+                    i+=1
+                    self.primeLabel.text = "Prime"
+                    }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
